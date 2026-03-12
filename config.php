978a14fb5d6c5f25b4402b9e3c592806838a5695
hiepquocchung5-blip <hiepquocchung5@gmail.com>
2026-03-12 06:40:27 +0630

    update domains

--- config.php
+++ config.php
@@ -20,7 +20,7 @@ try {
 }
 
 // API Configuration
-define('API_BASE_URL', 'https://gameapi.braix.online/');
+define('API_BASE_URL', 'https://slofinace.website/');
 
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
